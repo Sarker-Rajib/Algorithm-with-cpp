a0ed05f3c5d8d8469a764dde57efdff03abeeeca
Rajib Sarker <redhat0593@gmail.com>
2023-08-09 00:35:57 +0600

    q5 arranged]

--- 6.5-practice-module/05.dfs-height.cpp
+++ 6.5-practice-module/05.dfs-height.cpp
@@ -1,4 +1,26 @@
 /*  Question: You will be given an undirected graph as input.
     This graph will contain only one connected component. The edge number will be exactly node-1.
     Then take a node from the input and print its height. Consider root as 1.
+
+    in:                 out: height of 1 = 2
+    7
+    6
+    1 2
+    2 4
+    2 5
+    1 3
+    3 6
+    1 7
+    1
+
+    in:                 out: height of 7 = 0
+    7
+    6
+    1 2
+    2 4
+    2 5
+    1 3
+    3 6
+    1 7
+    7
 */
