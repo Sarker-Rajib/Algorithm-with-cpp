9e2683d0629e6860d967231eaa0e00581bdfea46
Rajib Sarker <redhat0593@gmail.com>
2023-08-09 00:40:54 +0600

    height problem solved

--- 6.5-practice-module/05.dfs-height.cpp
+++ 6.5-practice-module/05.dfs-height.cpp
@@ -24,3 +24,55 @@
     1 7
     7
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> adj[N];
+bool visited[N];
+
+int height[N];
+
+void dfs(int u)
+{
+    visited[u] = true;
+
+    for (auto v : adj[u])
+    {
+        if (visited[v] == true)
+        {
+            continue;
+        }
+        dfs(v);
+
+        if (height[v] + 1 > height[u])
+        {
+            height[u] = height[v] + 1;
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+    }
+
+    dfs(1);
+
+    int d;
+    cin >> d;
+
+    cout << "height of " << d << " = " << height[d];
+
+    // _____
+    return 0;
+}
