e36f98c40094505b8c56d46ffd9eed1db73ae8ea
Rajib Sarker <redhat0593@gmail.com>
2023-08-16 00:24:14 +0600

    counting room s

---
+++ 10.Dijkstra-algo-probs/Labyrinth.cpp
@@ -0,0 +1,28 @@
+/*
+5 8
+########
+#.A#...#
+#.##.#B#
+#......#
+########
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+const int N = 1e3 + 10;
+vector<pii> direc = {{0, -1},
+                     {0, 1},
+                     {-1, 0},
+                     {1, 0}};
+
+void bfs(){
+
+};
+
+int main()
+{
+
+    // __________
+    return 0;
+}
