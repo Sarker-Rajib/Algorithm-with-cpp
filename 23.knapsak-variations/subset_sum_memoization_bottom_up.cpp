03bcd88e4d2e5cc8fd7f55e4e17256ca5a96051f
Rajib Sarker <redhat0593@gmail.com>
2023-09-06 22:49:11 +0600

    subset - botto up

--- 23.knapsak-variations/subset_sum_memoization_bottom_up.cpp
+++ 23.knapsak-variations/subset_sum_memoization_bottom_up.cpp
@@ -1,72 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[1005][1005];
-
-void setV()
-{
-    for (int i = 0; i < 1005; i++)
-    {
-        for (int j = 0; j < 1005; j++)
-        {
-            dp[i][j] = -1;
-        }
-    }
-}
-
-bool subset_sum(int n, int set[], int s)
-{
-    if (n == 0)
-    {
-        if (s == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    if (dp[n][s] != -1)
-    {
-        return dp[n][s];
-    }
-
-    if (set[n - 1] <= s)
-    {
-        bool opt1 = subset_sum(n - 1, set, s - set[n - 1]);
-        bool opt2 = subset_sum(n - 1, set, s);
-        return dp[n][s] = opt1 || opt2;
-    }
-    else
-    {
-        return dp[n][s] = subset_sum(n - 1, set, s);
-    }
-};
-
 int main()
 {
     int n;
     cin >> n;
-    int set[n];
+    int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> set[i];
+        cin >> a[i];
     }
     int s;
     cin >> s;
 
-    setV();
+    bool dp[n + 1][s + 1];
+    dp[0][0] = true;
 
-    if (subset_sum(n, set, s))
+    for (int i = 1; i <= s; i++)
     {
-        cout << "YES";
+        dp[0][i] = false;
     }
-    else
+
+    for (int i = 1; i <= n; i++)
     {
-        cout << "NO";
+        for (int j = 0; j <= s; j++)
+        {
+            if (a[i - 1] <= s)
+            {
+                dp[i][j] = dp[i - 1][j - a[i - 1]] || dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
     }
+
+    dp[n][s] ? cout << "YES" : cout << "NO";
 
     // _____
     return 0;
